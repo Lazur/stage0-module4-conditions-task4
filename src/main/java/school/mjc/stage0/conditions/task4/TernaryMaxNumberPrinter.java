d617e0f29b6f599e27bdaa76f2db2776bf797dcc
Kyrylo_Popov <kyrylo_popov@epam.com>
2023-05-30 14:35:36 +0300

    Module 4 task4

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int greatest = second > first ? second : first; 
+        greatest = (third > greatest) ? third : greatest;
+        System.out.println(greatest);
     }
 }
